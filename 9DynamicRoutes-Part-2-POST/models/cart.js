652188c2123888140879cec20dcef9c01facd8e1
Shubham Pandhare <139115814+shubhampandhare1@users.noreply.github.com>
2023-09-03 15:42:58 +0530

    Dynamic Routes Part-2 (POST) Add-to-Cart

---
+++ 9DynamicRoutes-Part-2-POST/models/cart.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+        //fetch the previous cart
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { product: [], totalPrice: 0 };
+            if (!err) {
+                cart = JSON.parse(fileContent)
+            }
+            //Analyze the cart => Find existing product
+            const existingProductIndex = cart.product.findIndex(prod => prod.id === id);    ////
+            const existingProduct = cart.product[existingProductIndex];    ////
+            let updateProduct;
+            //add new product or increase the quantity
+            if (existingProduct) {
+                updateProduct = { ...existingProduct };
+                updateProduct.qty += 1;
+                cart.product = [...cart.product];   ////
+                cart.product[existingProductIndex] = updateProduct;     ////
+            }
+            else {
+                updateProduct = { id: id, qty: 1 };
+                cart.product = [...cart.product, updateProduct];
+            }
+            cart.totalPrice = cart.totalPrice + +productPrice; // extra '+' is to convert string to number
+            fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log(err);
+            })
+        })
+
+    }
+}
