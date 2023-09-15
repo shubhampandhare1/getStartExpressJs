f486641a7a28f06bd7226d92e8cb653c8de64d51
Shubham Pandhare <139115814+shubhampandhare1@users.noreply.github.com>
2023-09-15 12:05:09 +0530

    Create - Fetch - Retrieve - Update - Delete

---
+++ 17-18-Sequelize/controllers/shop.js
@@ -0,0 +1,85 @@
+const Product = require('../models/product');
+const Cart = require('../models/cart');
+
+exports.getProducts = (req, res, next) => {
+  Product.findAll()
+    .then((products) => {
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    });
+};
+
+//to get product id to show details page when click on details button
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  // Product.findAll({ where: { id: prodId } })
+  //   .then(products=>{
+  //     res.render('shop/product-detail',
+  //       {
+  //         product: products[0],
+  //         pageTitle: products[0].title,
+  //         path: '/products'
+  //       });
+  //   })
+  //   .catch(err => console.log(err))
+
+  Product.findByPk(prodId)
+    .then(product => {
+      res.render('shop/product-detail',
+        {
+          product: product,
+          pageTitle: product.title,
+          path: '/products'
+        });
+    })
+    .catch(err => console.log(err));
+}
+
+exports.getIndex = (req, res, next) => {
+  Product.findAll()
+    .then((products) => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId    //retrieve product id from incoming request on Add to cart
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  })
+  res.redirect('/cart');
+}
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
